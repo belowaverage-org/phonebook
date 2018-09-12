ff82fc71a83f54e5652177d598a1b53eff0578d0
Dylan Bickerstaff <dylan@belowaverage.org>
2018-09-12 17:03:10 -0400

    IMporting

--- api/reqs/import.api.php
+++ api/reqs/import.api.php
@@ -7,19 +7,19 @@ if(isset($_POST['import'])) {
 	$import = json_decode($_POST['import'], true);
 	if($import !== null) { //JSON Good
 		$schema = json_decode(file_get_contents(__DIR__.'/../schema.config.json'), true);
-		$data = array();
+		$objects = array();
+		$tags = array();
 		$unique = array();
 		foreach($import as $number => $object) { //For every number being imported...
 			if(!is_numeric($number)) { //If number is not numeric, continue to the next number
 				continue;
 			}
-			usleep(1);
-			$row = array( //Start the data in the row
-				'objectid' => uniqid(),
+			$row = array( //Start the objects in the row
+				'objectid' => bin2hex(random_bytes(5)),
 				'number' => $number
 			);
 			foreach($object as $attribute => $value) { //For every attribute in an import object, check that the attribute exists, otherwise do not add it to the row
-				if(isset($schema[$attribute])) { //If attribute 
+				if(isset($schema[$attribute]) && $attribute !== 'tags') { //If an attribute in the schema and not a tag list
 					if(isset($schema[$attribute]['type'])) { //Check type constraints
 						if($schema[$attribute]['type'] == 'number' && !ctype_digit($value)) { //Check number constraint
 							break;
@@ -32,26 +32,38 @@ if(isset($_POST['import'])) {
 						break;
 					}
 					$row[$attribute] = $value; //Add the attribute to the row
+				} elseif($attribute == 'tags') { //If a tag list
+					foreach($value as $tag) { //Foreach tag
+						if(!in_array($tag, $tags)) { //If not already in the array
+							array_push($tags, $tag); //Add to the array
+						}
+					}
 				}
 			}
-			array_push($data, $row); //Add the row to the data set
+			array_push($objects, $row); //Add the row to the objects set
 		}
-		foreach($data as $row) { //For each row in the data set
+		foreach($objects as $row) { //For each row in the objects set
+			$unique = array();
+			$objectid = '';
 			foreach($row as $attribute => $value) { //For each attribute in a row
 				if(isset($schema[$attribute]['unique']) && $schema[$attribute]['unique']) { //If attribute is unique according to the schema
-					if(!isset($unique[$attribute])) { //If unique attribute does not exist in the unique array
-						$unique[$attribute] = array(); //Create the unique attribute in the array as an array of data points
-					}
-					array_push($unique[$attribute], $value); //Push the value of the unique attributes from this row into the array
-					if(count($unique[$attribute]) >= $dbConfig['insertLoopLimit']) { //Every time the loop limit is reached reset the array and delete out of the database
-						$db->delete('objects', $unique);
-						unset($unique[$attribute]);
-					}
+					$unique[$attribute] = $value;
 				}
 			}
-			$db->delete('objects', $unique); //Delete the remainder
+			print_r($unique);
+			$objectid = $db->get('objects', array('objectid'), array('OR' => $unique));
+			if($objectid !== '') { //Already exists, so just modify the data instead of inserting
+				foreach($unique as $cname => $cvalue) {
+					unset($row[$cname]);
+				}
+				unset($row['objectid']);
+				$db->update('objects', $row, array('objectid' => $objectid));
+			} else { //Insert the data
+				$db->insert('objects', $row);
+			}
 		}
-		insertLoop('objects', $data); //Insert the new data
+		print_r($db->error());
+		//insertLoop('objects', $objects); //Insert the new objects
 	}
 }
 ?>
