18e95d54b94ea18337d7a47171db06177371a2a3
Dylan K. Bickerstaff <dylan@belowaverage.org>
2018-09-17 21:29:09 -0400

    Small changes

--- api/index.php
+++ api/index.php
@@ -2,14 +2,17 @@
 //September 2018 / Dylan Bickerstaff
 set_time_limit(300);
 $singlePointEntry = true;
-
-header('Author: Dylan Bickerstaff');
+header('phonebook-api-created-by: Dylan Bickerstaff');
+header('phonebook-api-version: 2.0');
+ob_start();
 if(isset($_POST['api']) && !empty($_POST['api'])) {
 	require('./reqs/database.api.php');
 	if($_POST['api'] == 'import') {
 		require('./reqs/import.api.php');
 	}
-	exit;
+} else {
+	echo file_get_contents('api.documentation.htm');
 }
-echo file_get_contents('api.documentation.htm');
+header('phonebook-api-response-time: '.(microtime(true) - $_SERVER['REQUEST_TIME_FLOAT']));
+ob_end_flush();
 ?>
---
+++ api/reqs/library.api.php
@@ -0,0 +1,5 @@
+<?php
+if(!isset($singlePointEntry)) { http_response_code(403); exit; }
+
+
+?>
