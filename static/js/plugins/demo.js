6ff0dc85f2b4dd377b9ce01b7490a3e3d80c63ee
Dylan Bickerstaff <dylan@belowaverage.org>
2019-06-11 14:42:22 -0400

    More demo stuff

--- static/js/plugins/demo.js
+++ static/js/plugins/demo.js
@@ -2,12 +2,17 @@ $(document).on('bsloaded', function() {
    $('<link rel="stylesheet" href="static/css/demo.css">').appendTo('head');
     typeOut(
         'Hi!--------+--------' +
-        'Welcome to the Phone Book!'
-    , 60);
+        'W---elc-ome t---o th-e Pho--ne Book!----------------^^' +
+        'To begin a search:+Just start typing!-------------------^^' +
+        'You can find more functions like:+Printing+Exporting+and Help+in this menu to the left.'
+    , 60, function() {
+        $('#hamopen').click();
+    });
 });
 
-function typeOut(text, delay) {
+function typeOut(text, delay, callback) {
     var string = new String(text);
+    setTimeout(callback, (string.length * delay));
     for(var count = 0; string.length > count; count++) {
         (function() {
             var ct = count;
@@ -22,6 +27,15 @@ function typeOut(text, delay) {
                 }
                 if(string[ct] == '~') {
                     $('#input > span:last-child').addClass('valid');
+                    return;
+                }
+                 if(string[ct] == '^') {
+                    if($('#input > span').length == 1) {
+                        $('#input > span:last-child').text('');
+                    } else {
+                        $('#input > span:last-child').remove();
+                    }
+                    $('#input > span:last-child').addClass('type');
                     return;
                 }
                 $('#input > span:last-child').append(string[ct]);
