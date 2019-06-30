6b980fd7ee416c369bd3300ca2f3e86eee72a643
Dylan K. Bickerstaff <dylan@belowaverage.org>
2019-06-30 08:53:23 -0400

    Add comments.

--- api/reqs/search.api.php
+++ api/reqs/search.api.php
@@ -10,6 +10,9 @@ Searches the database for objects using search tags.
 */
 if(!isset($singlePointEntry)){http_response_code(403);exit;}
 if(isset($_POST['search']) && !empty($_POST['search'])) {
+    //Initialize Variables.
+    $count = 100;
+    $offset = 0;
     $blankJson = '{"tags":[],"objects":{}}';
     $validSearchTags = array();
     $tagSearchObjects = array();
@@ -21,46 +24,49 @@ if(isset($_POST['search']) && !empty($_POST['search'])) {
             'tags_objects.objectid' => 'objectid'
         )
     );
-    $searchTags = json_decode($_POST['search'], true);
-    $count = 100;
-    $offset = 0;
+    $searchTags = json_decode($_POST['search'], true); //JSON decode search input.
     if(isset($_POST['count']) && is_numeric($_POST['count'])) {
         $count = $_POST['count'];
     }
     if(isset($_POST['offset']) && is_numeric($_POST['offset'])) {
         $offset = $_POST['offset'];
     }
-    if($searchTags !== null) {
-        foreach($searchTags as $tag) {
-            if(!empty($tag) && ctype_alpha($tag) && $tag !== '' && strlen($tag) >= 2) {
-                array_push($validSearchTags, $tag);
+    if($searchTags !== null) { //If search input is valid JSON.
+        foreach($searchTags as $tag) { //For each tag in the search input.
+            if(!empty($tag) && ctype_alpha($tag) && $tag !== '' && strlen($tag) >= 2) { //If tag is longer than 2 characters and only contains alpha characters.
+                array_push($validSearchTags, $tag); //Add the tag to the validSearchTags array.
             }
         }
-        if(count($validSearchTags) == 1) {
-            $objects = $db->select('tags', $objectJoin, '*', array(
+        if(count($validSearchTags) == 1) { //If there is only one tag present.
+            $objects = $db->select('tags', $objectJoin, '*', array( //Search the database with a wildcard appeneded to the tag.
                 'tags.text[~]' => $validSearchTags[0].'%'
             ));
-        } else {
-            $objects = $db->select('tags', $objectJoin, '*', array(
+        } else { //If there are more than 1 search tags.
+            $objects = $db->select('tags', $objectJoin, '*', array( //Search the database for objects associated with the valid tags.
                 'tags.text' => $validSearchTags,
                 'GROUP' => 'tags_objects.objectid',
                 'HAVING' => $db->raw('COUNT(tags_objects.objectid) = '.count($validSearchTags))
             ));
         }
-        if(is_array($objects) && !empty($objects)) {
-            if(isset($_POST['sort']) && !empty($_POST['sort']) && isset(SCHEMA[$_POST['sort']])) {
+        if(is_array($objects) && !empty($objects)) { //If the search returned objects.
+            /*
+
+                SORT IS FLAWED: DOES NOT SORT BY ALPHA ONLY NUMERIC.
+
+            */
+            if(isset($_POST['sort']) && !empty($_POST['sort']) && isset(SCHEMA[$_POST['sort']])) { //If a sort is specified from the post request.
                 usort($objects, function($a, $b) {
-                    if(is_numeric($a[$_POST['sort']])) {
+                    if(is_numeric($a[$_POST['sort']])) { //Sort the objects based on the attribute provided in the "sort" post request.
                         return $a[$_POST['sort']] - $b[$_POST['sort']];
                     } else {
                         return strcmp($a[$_POST['sort']], $b[$_POST['sort']]);
                     }
                 });
             }
-            if(isset($_POST['order']) && $_POST['order'] == 1) {
+            if(isset($_POST['order']) && $_POST['order'] == 1) { //Reverse the order if "order" equals "1".
                 $objects = array_reverse($objects);
             }
-            foreach($objects as $k => $object) {
+            foreach($objects as $k => $object) { //For each object in the array, remove the tagid, text, and objectid fields, and apply a count and offset.
                 $objectid = $object['objectid'];
                 unset($object['tagid']);
                 unset($object['text']);
@@ -73,7 +79,7 @@ if(isset($_POST['search']) && !empty($_POST['search'])) {
                     $offset--;
                 }
             }
-            $filteredTags = $db->select('tags_objects', array(
+            $filteredTags = $db->select('tags_objects', array( //Search the database for tags associated with the searched results.
                 '[>]tags' => array(
                      'tags_objects.tagid' => 'tagid'
                 )
@@ -83,17 +89,17 @@ if(isset($_POST['search']) && !empty($_POST['search'])) {
                 'tags_objects.objectid' => $tagSearchObjects,
                 'GROUP' => 'tags.text'
             ));
-            if(is_array($filteredTags)) {
-                foreach($filteredTags as $k => $tag) {
+            if(is_array($filteredTags)) { //If filteredTags is an array.
+                foreach($filteredTags as $k => $tag) { //Remove other database attributes like objectid and only use the tag text.
                     $filteredTags[$k] = $tag['text'];
                 }
-                if($objects !== false && $filteredTags !== false) {
-                    echo json_encode(array(
+                if($objects !== false && $filteredTags !== false) { //If objects and filteredTags is valid.
+                    echo json_encode(array( //Return the results from this search request.
                         'tags' => $filteredTags,
                         'objects' => $organizedObjects
                     ), $prettyPrintIfRequested);
                 }
-            } else {
+            } else { //Otherwise return a blank response.
                 echo $blankJson;
             }
         } else {
