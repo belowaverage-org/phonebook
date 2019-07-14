3fc14ea6be3c687a996f3882588b00e7575dd08b
Dylan K. Bickerstaff <dylan@belowaverage.org>
2019-07-14 12:29:22 -0400

    Updated search api (again)

--- api/reqs/search.api.php
+++ api/reqs/search.api.php
@@ -12,26 +12,30 @@ if(!isset($singlePointEntry)){http_response_code(403);exit;}
 if(isset($_POST['search']) && !empty($_POST['search'])) {
     require_once('library.lib.php');
     //Initialize Variables.
-    $count = 100;
-    $offset = 0;
-    $blankJson = '{"tags":[],"objects":{}}';
+    $blankJson = '{"objects":{}}';
     $searchTags = array();
     $tagSearchObjects = array();
     $organizedObjects = array();
     $searchQuery = json_decode($_POST['search'], true); //JSON decode search input.
-    if(isset($_POST['count']) && is_numeric($_POST['count'])) {
-        $count = $_POST['count'];
-    }
-    if(isset($_POST['offset']) && is_numeric($_POST['offset'])) {
-        $offset = $_POST['offset'];
-    }
     if($searchQuery !== null) { //If search input is valid JSON.
         $databaseQuery = array();
-        if(isset($searchQuery['WHERE']) && !empty($searchQuery['WHERE'])) { //If WHERE is specified, add it to the query.
-            $databaseQuery = array_merge($databaseQuery, $searchQuery['WHERE']);
+        $databaseCols = array('objects.objectid');
+        $queryContainsTags = (isset($searchQuery['SEARCH']['TAGS']) && !empty($searchQuery['SEARCH']['TAGS']));
+        if(isset($searchQuery['OUTPUT']['ATTRIBUTES']) && !empty($searchQuery['OUTPUT']['ATTRIBUTES'])) {
+            $databaseCols = array_merge($databaseCols, $searchQuery['OUTPUT']['ATTRIBUTES']);
         }
-        if(isset($searchQuery['TAGS']) && !empty($searchQuery['TAGS'])) {
-            foreach($searchQuery['TAGS'] as $tag) { //For each tag in the search input.
+        if(isset($searchQuery['SEARCH']) && !empty($searchQuery['SEARCH'])) { //If WHERE is specified, add it to the query.
+            if($queryContainsTags) { //Remove tags from array, because the rest can be understood my MEDOO
+                $tags = $searchQuery['SEARCH']['TAGS'];
+                unset($searchQuery['SEARCH']['TAGS']);
+            }
+            $databaseQuery = array_merge($databaseQuery, $searchQuery['SEARCH']); //Merge the posted query into the database query.
+            if($queryContainsTags) { // Add tags back into the array for later.
+                $searchQuery['SEARCH']['TAGS'] = $tags;
+            }
+        }
+        if($queryContainsTags) {
+            foreach($searchQuery['SEARCH']['TAGS'] as $tag) { //For each tag in the search input.
                 if(!empty($tag) && ctype_alpha($tag) && $tag !== '' && strlen($tag) >= 2) { //If tag is longer than 2 characters and only contains alpha characters.
                     array_push($searchTags, $tag); //Add the tag to the $searchTags array.
                 }
@@ -49,23 +53,26 @@ if(isset($_POST['search']) && !empty($_POST['search'])) {
                 ), '[>]objects' => array(
                     'tags_objects.objectid' => 'objectid'
                 )
-            ), '*', $databaseQuery);
+            ), $databaseCols, $databaseQuery);
         } else {
-            $objects = $db->select('objects', '*', $databaseQuery); //Query the objects table.
+            $objects = $db->select('objects', $databaseCols, $databaseQuery); //Query the objects table.
         }
         if(is_array($objects) && !empty($objects)) { //If the search returned objects.
-            $organizedObjects = organizeDatabaseObjects($objects);
-            $filteredTags = $db->select('tags_objects', array( //Search the database for tags associated with the searched results.
-                '[>]tags' => array(
-                     'tags_objects.tagid' => 'tagid'
-                )
-            ), array(
-                'tags.text'
-            ), array(
-                'tags_objects.objectid' => array_keys($organizedObjects),
-                'GROUP' => 'tags.text'
-            ));
-            if(is_array($filteredTags)) { //If filteredTags is an array.
+            $includeTags = (isset($searchQuery['OUTPUT']['OPTIONS']) && in_array('showObjectTags', $searchQuery['OUTPUT']['OPTIONS']));
+            $organizedObjects = organizeDatabaseObjects($objects, $includeTags);
+            if(isset($searchQuery['OUTPUT']['OPTIONS']) && in_array('showAvailableTags', $searchQuery['OUTPUT']['OPTIONS'])) {
+                $filteredTags = $db->select('tags_objects', array( //Search the database for tags associated with the searched results.
+                    '[>]tags' => array(
+                         'tags_objects.tagid' => 'tagid'
+                    )
+                ), array(
+                    'tags.text'
+                ), array(
+                    'tags_objects.objectid' => array_keys($organizedObjects),
+                    'GROUP' => 'tags.text'
+                ));
+            }
+            if(isset($filteredTags) && is_array($filteredTags)) { //If filteredTags is an array.
                 foreach($filteredTags as $k => $tag) { //Remove other database attributes like objectid and only use the tag text.
                     $filteredTags[$k] = $tag['text'];
                 }
@@ -76,7 +83,9 @@ if(isset($_POST['search']) && !empty($_POST['search'])) {
                     ), $prettyPrintIfRequested);
                 }
             } else { //Otherwise return a blank response.
-                echo $blankJson;
+                echo json_encode(array( //Return the results from this search request.
+                        'objects' => $organizedObjects
+                ), $prettyPrintIfRequested);
             }
         } else {
             echo $blankJson;
